3dc69ee14df603c6a087de25c53c100f1208a407
Alexander Nikolin <hsehowuhh@gmail.com>
2022-02-24 03:41:43 +0300

    Update setup.py
    fix dm_control installation issue as in https://github.com/rail-berkeley/d4rl/issues/141

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
                       'termcolor',  # adept_envs dependency
                       'click',  # adept_envs dependency
                       'dm_control' if 'macOS' in platform() else
-                      'dm_control @ git+git://github.com/deepmind/dm_control@master#egg=dm_control',
+                      'dm_control @  git+https://github.com/deepmind/dm_control@4f1a9944bf74066b1ffe982632f20e6c687d45f1',
                       'mjrl @ git+git://github.com/aravindr93/mjrl@master#egg=mjrl'],
     packages=find_packages(),
     package_data={'d4rl': ['locomotion/assets/*',
